22a1cdf73616d8f0b27eda364a37847969160b70
sya5jv <sya5jv@virginia.edu>
2023-03-26 01:14:02 -0400

    initial commit

--- unforgettable/App.js
+++ unforgettable/App.js
@@ -1,45 +1,4 @@
 import { StatusBar } from 'expo-status-bar';
-<<<<<<< HEAD
-import { StyleSheet, Text, View, Image, ScrollView, TextInput } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { HomeScreen } from "./components/HomePage.js";
-import Swiper from "react-native-swiper";
-import { openDatabase } from 'react-native-sqlite-storage';
-
-const Stack = createNativeStackNavigator();
-
-// Variable to access database
-var db = openDatabase({ name: 'UserProfile.db' });
-
-
-export default function App() {
-  return (
-    <Swiper showsButtons={true}>
-      <Swiper
-          horizontal={false}
-          loop={false}
-          showsButtons={true}>
-          <View>
-            <Text>TOP</Text>
-          </View>
-          <View>
-          <Text>HOME</Text>
-          </View>
-          <View>
-          <Text>BOTTOM</Text>
-          </View>
-        </Swiper>     
-      <View>
-        <Text>RIGHT1</Text>
-      </View>
-      <View>
-        <Text>RIGHT2</Text>
-      </View>
-    </Swiper>
-  )
-}
-=======
 import {
 	StyleSheet,
 	Text,
@@ -90,4 +49,3 @@ export default function App() {
 		</Swiper>
 	);
 }
->>>>>>> 7f56f75f63033194f6d4dabff2e55aab6f965dd8
