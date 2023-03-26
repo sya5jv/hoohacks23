b6deac470c37a46cfd3d0eaef552cf6da2188914
sya5jv <sya5jv@virginia.edu>
2023-03-26 06:23:32 -0400

    added home page

--- unforgettable/src/App.js
+++ unforgettable/src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import Home from './Components/Home.js';
 
 function App() {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div style={{ textAlign: 'center' }}>
+			<Home />
+			<header>
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
---
+++ unforgettable/src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    let name = "Home page";
+
+    return (
+        <div>
+            <h1>{name}</h1>
+            <p>This is the homepage.</p>
+        </div>
+    );
+};
+
+export default Home;
