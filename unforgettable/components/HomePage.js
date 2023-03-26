3150f23a71042acaf50e5112970fbb17768d1bf3
sya5jv <sya5jv@virginia.edu>
2023-03-26 05:55:57 -0400

    remove componenets

--- unforgettable/components/HomePage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {View, Text, Image, ScrollView, TextInput} from 'react-native';
-
-const HomeScreen = ({navigation}) => {
-    return (
-      <Text>MEAT MAN</Text>
-    );
-  };
-  export {HomeScreen}
