bacd490cb07095bf7767ad64946c2cb043a4eb51
jbilbro01 <jbilbro01@gmail.com>
2023-03-25 22:31:14 -0400

    committing missing package files for expo

--- unforgettable/App.js
+++ unforgettable/App.js
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, TextInput } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { HomeScreen } from "./components/HomePage.js";
+import Swiper from "react-native-swiper";
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
+  // return (
+  //   <NavigationContainer>
+  //     <Stack.Navigator>
+  //       <Stack.Screen
+  //           name="Homer"
+  //           component={HomeScreen}
+  //           options={{title: 'Welcome'}}
+  //         />
+  //     </Stack.Navigator>
+  //   </NavigationContainer>
+  // );
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Swiper showsButtons={true}>
+      <Swiper
+          horizontal={false}
+          loop={false}>
+          <View>
+            <Text>TOP</Text>
+          </View>
+          <View>
+          <Text>HOME</Text>
+          </View>
+          <View>
+          <Text>BOTTOM</Text>
+          </View>
+        </Swiper>     
+      <View>
+        <Text>RIGHT1</Text>
+      </View>
+      <View>
+        <Text>RIGHT2</Text>
+      </View>
+    </Swiper>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
