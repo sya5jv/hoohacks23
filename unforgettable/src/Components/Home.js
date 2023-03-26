2ba5100180a885f3a271935a9d1f688d83b1199a
sya5jv <sya5jv@virginia.edu>
2023-03-26 07:32:07 -0400

    get rid of sql

--- unforgettable/src/Components/Home.js
+++ unforgettable/src/Components/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
 const Home = () => {
-    let name = "Home page";
+    let title = "Home page";
+    let temp_name = "Sam";
+    let welcome_message = "Welcome back, ";
+
 
     return (
         <div>
-            <h1>{name}</h1>
+            <h1>{title}</h1>
             <p>This is the homepage.</p>
+
+            <h2>{welcome_message}{temp_name}</h2>
         </div>
     );
 };
