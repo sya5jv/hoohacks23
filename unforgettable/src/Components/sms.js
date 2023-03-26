77948e18285fe97c2425adcc4a73120e8eaef11e
OGHyper <firefoxphil@hotmail.com>
2023-03-26 06:45:33 -0400

    committing sms.js second time

--- unforgettable/src/Components/sms.js
+++ unforgettable/src/Components/sms.js
@@ -1,9 +1,9 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(ACcf914d409cb3ddc2497eb3ca5f4552fe, fb47c962ec8ec68bb6cb59997671b98d);
+const accountSid = 'ACcf914d409cb3ddc2497eb3ca5f4552fe';
+const authToken = 'fb47c962ec8ec68bb6cb59997671b98d';
+const client = require('twilio')(accountSid, authToken);
 
 client.messages
   .create({
