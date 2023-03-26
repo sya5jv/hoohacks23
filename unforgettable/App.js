46b0e6575372357638530ea04a15002060e69f60
sya5jv <sya5jv@virginia.edu>
2023-03-26 01:37:57 -0400

    incomplete app.js db setup

--- unforgettable/App.js
+++ unforgettable/App.js
@@ -10,6 +10,7 @@ import {
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from './components/HomePage.js';
+import { SetupPage } from './components/SetupPage.js';
 import Swiper from 'react-native-swiper';
 import React from 'react';
 
@@ -27,6 +28,17 @@ export default function App() {
 	//     </Stack.Navigator>
 	//   </NavigationContainer>
 	// );
+
+	const sqlite3 = require('sqlite3').verbose();
+	let db = new sqlite3.Database("./UserProfile.db");
+	let count = "SELECT count(*) FROM table";
+	let cursor = db.rawQuery(count, null);
+	cursor.moveToFirst();
+	let icount = cursor.getInt(0);
+	if (icount > 0) {
+		
+	}
+
 	return (
 		<Swiper showsButtons={true}>
 			<Swiper horizontal={false} loop={false}>
